42bf7af6fe640595b2e28edd0ac3d1ef26b2974e
Jessie S <jessie.sholler@gmail.com>
2019-11-25 15:08:07 -0500

    redux files started

---
+++ client/src/Store/Actions/Types.js
@@ -0,0 +1,22 @@
+// Splash page actions 
+
+// Navbar Actions 
+export const getUserDetials = "getUserDetails"
+export const signOut= "signOut"
+export const retrieveAlerts = "retrieveAlerts"
+export const removeAlerts = "removeAlerts"
+
+// Homepage Actions 
+export const getCategories = "getCategories"
+export const getListedApps = "getListedApps"
+export const getAppDetailsCard = "getAppNames"
+export const getAppDetailsDropdown = "getAppNames"
+export const viewCategory = "viewCategory"
+export const viewApp = "viewApp"
+export const displayShield = "displayShields"
+export const submitAppSuggestion = "submitAppSuggestion"
+export const rednerTrending = "renderTrending"
+
+// AppPage actions 
+
+// Category Actions 
