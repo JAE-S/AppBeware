ac7ff1c81a3f9928f152a79ff934d483d0d7e2f5
Scott Moore <woodwindscott@yahoo.com>
2019-11-25 17:32:27 -0500

    Starter files for Redux

---
+++ client/src/Store/Reducers/categoryReducer.js
@@ -0,0 +1,24 @@
+import { VIEW_ALL_CATEGORIES, VIEW_SINGLE_CATEGORY } from "../actions/new-types";
+
+const initialState = {
+    allCategories: [],
+    singleCategory: []
+};
+
+export default function(state=initialState, action) {
+
+    switch(action.type) {
+
+        case VIEW_ALL_CATEGORIES:
+            return {
+                ...state,
+                allCategories: action.payload
+            }
+
+        
+
+
+        default: 
+            return state;
+    }
+}
