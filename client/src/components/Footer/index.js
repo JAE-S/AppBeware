81c75baa19d222efa42b6a450d757ba32f785739
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2019-11-26 09:23:49 -0500

    added sign out back to footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,34 +1,9 @@
-// Import React
-// =========================================================
-   import React from 'react';
-// Import Material UI Styles
-// =========================================================
-   import { makeStyles } from '@material-ui/core/styles';
-// Import Material UI Components
-// =========================================================
-   import { CssBaseline, Typography, Container, Link} from '@material-ui/core/';
-
-
-// Styles
-// =========================================================
-    const useStyles = makeStyles(theme => ({
-      root: {
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '15vh',
-      },
-      main: {
-        marginTop: theme.spacing(8),
-        marginBottom: theme.spacing(2),
-      },
-      footer: {
-        color: "#f7f7f7",
-        padding: theme.spacing(3, 2),
-        marginTop: 'auto',
-        backgroundColor: "#56585D",
-        textAlign: 'center'
-      },
-    }));
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 
 function Copyright() {
   return (
@@ -43,10 +18,35 @@ function Copyright() {
         Disclaimer
       </Link>
 
-// Export StickyFooter
-// =========================================================
-  export default function StickyFooter() {
-    const classes = useStyles();
+
+      {' | Sign Out'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    //  minHeight: '15vh',
+   
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    color: "#ffffff",
+    padding: theme.spacing(3, 2),
+    marginTop: 'auto',
+    backgroundColor: "#56585D",
+    textAlign: 'center'
+           
+  },
+}));
+
+export default function StickyFooter() {
+  const classes = useStyles();
 
   return (
     <div className={classes.root}>
@@ -60,3 +60,4 @@ function Copyright() {
     </div>
   );
 }
+
