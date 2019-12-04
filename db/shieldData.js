a9f27b3b872d1a6c6d402330f2f509595526f8f0
Scott Moore <woodwindscott@yahoo.com>
2019-12-04 10:43:25 -0500

    Shield image rendering, Moment for last updated

--- db/shieldData.js
+++ db/shieldData.js
@@ -1,10 +1,10 @@
 const shieldData =
 [
-    { "name": "Predator Risk","icon": "PR_icon","altText": "Predator Risk icon","info": "Why are you a predator Risk?"},
-    { "name": "Dangerous Behavior","icon": "DB_icon","altText": "Dangerous Behavior icon","info": "Take the pod challenge, it will turn your mouth blue!"},
-    { "name": "Violent Content","icon": "VC_icon","altText": "Violent Content icon","info": "This is violent content."},
-    { "name": "Cyberbullying","icon": "CB_icon","altText": "Cyberbullying icon","info": "Don't cyberbully me!"},
-    { "name": "Sexual Content","icon": "SC_icon","altText": "Sexual Content icon","info": "Sexual content?"},
+    { "name": "Predator Risk","icon": "https://imagizer.imageshack.com/img921/8898/PyRetH.png","altText": "Predator Risk icon","info": "Apps that expose children to predators through solicitation, sharing personal information, encouragement to meet in person, intent to engage in sexual activity, stalking or harm."},
+    { "name": "Dangerous Behavior","icon": "https://imagizer.imageshack.com/img922/4586/FJpyql.png","altText": "Dangerous Behavior icon","info": "Apps that promote self harm, suicide, eating disorders, choking games, dangerous challenges or other acts where serious injury or death may result."},
+    { "name": "Violent Content","icon": "https://imagizer.imageshack.com/img923/3872/QmCYSy.png","altText": "Violent Content icon","info": "Apps that depict or facilitate gratuitous violence or other dangerous activities against people or animals.  These include threats, gun violence, terrorism, use, sale or depictions of explosives or firearms."},
+    { "name": "Cyberbullying","icon": "https://imagizer.imageshack.com/img922/7184/zsAk82.png","altText": "Cyberbullying icon","info": "Harassment in the comments and parody videos being created to humiliate and tease some users, particularly users with disabilities. Bullying people from different religious or ethnic backgrounds, blackmail, extortion, public humiliation or harassment."},
+    { "name": "Sexual Content","icon": "https://imagizer.imageshack.com/img921/1830/Jad3lH.png","altText": "Sexual Content icon","info": "Apps that contain or promote sexual content, such as nudity, pornography, or any content or services intended to be sexually gratifying."},
 ]
 
 module.exports = shieldData;
