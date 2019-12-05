d1bddc422c09b00d70399a84f7210ffd5111651f
Scott Moore <woodwindscott@yahoo.com>
2019-12-04 19:24:13 -0500

    Updates to category actions for single category info and render properly on page

--- client/src/Store/Actions/actions.js
+++ client/src/Store/Actions/actions.js
@@ -1,5 +1,6 @@
 export const VIEW_ALL_CATEGORIES = 'VIEW_ALL_CATEGORIES';
 export const VIEW_SINGLE_CATEGORY = 'VIEW_SINGLE_CATEGORY';
+export const VIEW_SINGLE_CATEGORY_INFO = "VIEW_SINGLE_CATEGORY_INFO";
 
 export function viewAllCategories(allCategories) {
     return {
@@ -12,5 +13,12 @@ export function viewSingleCategory(singleCategory) {
     return {
         type: VIEW_SINGLE_CATEGORY,
         singleCategory: singleCategory
+    }
+}
+
+export function viewSingleCategoryInfo(singleCategoryInfo) {
+    return {
+        type: VIEW_SINGLE_CATEGORY_INFO,
+        singleCategoryInfo: singleCategoryInfo
     }
 }
--- client/src/pages/AppPage.js
+++ client/src/pages/AppPage.js
@@ -1,6 +1,8 @@
 // Import React
 // =========================================================
-    import React from "react"; 
+    import React, { Component } from "react"; 
+    import { connect } from "react-redux";
+
 // Import Material UI components 
 // =========================================================
     import { Grid, Typography, Button, Table, TableBody, TableRow, TableCell }from '@material-ui/core/';
@@ -15,6 +17,8 @@
     import Modal from "../components/Modals"
     import AddAppReview from "../components/AddAppReview"
     import {AppRatings, CustomizedRatings }from "../components/Ratings"
+    import { viewSingleApp } from "../Store/Actions/appActions";
+
 // Import Media
 // =========================================================
     import PR_icon from "../assets/images/shields/predator_risk.png";
@@ -117,127 +121,149 @@
         ) 
     }
       
-// Export Default App Page Function
-// =========================================================
-    export default function App() {
-   
-        return (
-            <>
-                <Nav/>
-                <main>
-                    <div className="header"> 
-                        <Grid container spacing={4}>
-                
-                            <Grid align="center" item xs={12} sm={3}>
-                                <img alt={data[0].name} style={{ width: "100%", height: "auto", borderRadius: 16 }} src={data[0].logoUrl}/>
-                            </Grid> 
-                            <Grid item xs={12}  sm={9}>
-                                <h1 style={{ borderBottom: "1px solid grey", marginRight: "20px"}}>{data[0].name}
-                                {<AddAlert style={{ paddingLeft: 20}} />}
-                                </h1>
-                                <p>{data[0].description}</p>
-                                <Button href={data[0].link} target="_blank">
-                                    <Typography variant="caption" color="textSecondary">
-                                        View in app store
-                                    </Typography>
-                                </Button>
-                            </Grid> 
-                    
-                        </Grid>
-                    </div>
-         
-                    <Wrapper style={{paddingTop: 80}}> 
-                    {/* Rating -> Danger rating & Shield Tracker */}
-                        <Grid container spacing={4}>
-                    
-                            <Grid item xs={12} sm={6}>
-                                <h3 align="center" >Danger Rating</h3>
-                                <SmallGrid 
-                                    smallGirdLeft={<Brightness1Rounded style={{ fontSize: "36px", color: "red"}} />}
-                                    smallGirdRight={<AppRatings ratingValue={5}/>}
-                                />
-                                <SmallGrid 
-                                    smallGirdLeft={<Brightness1Rounded style={{ fontSize: "36px", color: "red"}} />}
-                                    smallGirdRight={<AppRatings ratingValue={4}/>}
-                                />
-                                <SmallGrid 
-                                    smallGirdLeft={<Brightness1Rounded style={{ fontSize: "36px", color: "red"}} />}
-                                    smallGirdRight={<AppRatings ratingValue={3}/>}
-                                />
-                                <SmallGrid 
-                                    smallGirdLeft={<Brightness1Rounded style={{ fontSize: "36px", color: "red"}} />}
-                                    smallGirdRight={<AppRatings ratingValue={2}/>}
-                                />
-                                <SmallGrid 
-                                    smallGirdLeft={<Brightness1Rounded style={{ fontSize: "36px", color: "red"}} />}
-                                    smallGirdRight={<AppRatings ratingValue={1}/>}
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <h3 align="center" >Shield Tracker</h3>
-                                <SmallGrid 
-                                    smallGirdLeft={<img alt="Predator Risk" style={{  justifyContent: "center", maxWidth: "40px", width: "100%", height: "auto"}} src={PR_icon}/>}
-                                    smallGirdRight={<CustomizedRatings />}
-                                />
-                                <SmallGrid 
-                                    smallGirdLeft={<img alt="Dangerous Behavior" style={{ maxWidth: "40px", width: "100%", height: "auto"}} src={DB_icon}/>}
-                                    smallGirdRight={<CustomizedRatings />}
-                                />
-                                <SmallGrid 
-                                    smallGirdLeft={<img alt="Cyberbullying" style={{ maxWidth: "40px", width: "100%", height: "auto"}} src={CB_icon} />}
-                                    smallGirdRight={<CustomizedRatings />}
-                                />
-                                <SmallGrid 
-                                    smallGirdLeft={<img alt="Violent Content" style={{ maxWidth: "40px", width: "100%", height: "auto"}} src={VC_icon}/>}
-                                    smallGirdRight={<AppRatings />}
-                                />
-                                <SmallGrid 
-                                    smallGirdLeft={<img alt="Sexual Content" style={{ maxWidth: "40px", width: "100%", height: "auto"}} src={SC_icon} />}
-                                    smallGirdRight={<AppRatings />}
-                                />
-                            </Grid>
 
-                        </Grid>
-                    
-                        {/* Comments */}
-                        <Grid style={{ display: "flex", alignItems: "center", borderBottom: "2px solid grey", height: "54px"}}container spacing={0}>
-                            <Grid item xs={10}>
-                                <h2 >Reviews: </h2>
+// MAIN App Page Function
+// =========================================================
+    class App extends Component {
+   
+        render() {
+            return (
+                <>
+                    <Nav/>
+                    <main>
+                        <div className="header"> 
+                            <Grid container spacing={4}>
+
+
+
+
+
+
+                                <Grid align="center" item xs={12} sm={3}>
+                                    <img alt={data[0].name} style={{ width: "100%", height: "auto", borderRadius: 16 }} src={data[0].logoUrl}/>
+                                </Grid> 
+                                <Grid item xs={12}  sm={9}>
+                                    <h1 style={{ borderBottom: "1px solid grey", marginRight: "20px"}}>{data[0].name}
+                                    {<AddAlert style={{ paddingLeft: 20}} />}
+                                    </h1>
+                                    <p>{data[0].description}</p>
+                                    <Button href={data[0].link} target="_blank">
+                                        <Typography variant="caption" color="textSecondary">
+                                            View in app store
+                                        </Typography>
+                                    </Button>
+                                </Grid> 
+
+
+
+
+                        
                             </Grid>
-                            <Grid align="right" item xs={2}>
-                            <Modal
-                                modalTitle={` Share your concerns about ${data[0].name}.`}
-                                openModal="Add Review"
-                                modalBody={<AddAppReview/>}
-                                modalButton1="Submit"
+                        </div>
+            
+                        <Wrapper style={{paddingTop: 80}}> 
+                        {/* Rating -> Danger rating & Shield Tracker */}
+                            <Grid container spacing={4}>
+                        
+                                <Grid item xs={12} sm={6}>
+                                    <h3 align="center" >Danger Rating</h3>
+                                    <SmallGrid 
+                                        smallGirdLeft={<Brightness1Rounded style={{ fontSize: "36px", color: "red"}} />}
+                                        smallGirdRight={<AppRatings ratingValue={5}/>}
+                                    />
+                                    <SmallGrid 
+                                        smallGirdLeft={<Brightness1Rounded style={{ fontSize: "36px", color: "red"}} />}
+                                        smallGirdRight={<AppRatings ratingValue={4}/>}
+                                    />
+                                    <SmallGrid 
+                                        smallGirdLeft={<Brightness1Rounded style={{ fontSize: "36px", color: "red"}} />}
+                                        smallGirdRight={<AppRatings ratingValue={3}/>}
+                                    />
+                                    <SmallGrid 
+                                        smallGirdLeft={<Brightness1Rounded style={{ fontSize: "36px", color: "red"}} />}
+                                        smallGirdRight={<AppRatings ratingValue={2}/>}
+                                    />
+                                    <SmallGrid 
+                                        smallGirdLeft={<Brightness1Rounded style={{ fontSize: "36px", color: "red"}} />}
+                                        smallGirdRight={<AppRatings ratingValue={1}/>}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <h3 align="center" >Shield Tracker</h3>
+                                    <SmallGrid 
+                                        smallGirdLeft={<img alt="Predator Risk" style={{  justifyContent: "center", maxWidth: "40px", width: "100%", height: "auto"}} src={PR_icon}/>}
+                                        smallGirdRight={<CustomizedRatings />}
+                                    />
+                                    <SmallGrid 
+                                        smallGirdLeft={<img alt="Dangerous Behavior" style={{ maxWidth: "40px", width: "100%", height: "auto"}} src={DB_icon}/>}
+                                        smallGirdRight={<CustomizedRatings />}
+                                    />
+                                    <SmallGrid 
+                                        smallGirdLeft={<img alt="Cyberbullying" style={{ maxWidth: "40px", width: "100%", height: "auto"}} src={CB_icon} />}
+                                        smallGirdRight={<CustomizedRatings />}
+                                    />
+                                    <SmallGrid 
+                                        smallGirdLeft={<img alt="Violent Content" style={{ maxWidth: "40px", width: "100%", height: "auto"}} src={VC_icon}/>}
+                                        smallGirdRight={<AppRatings />}
+                                    />
+                                    <SmallGrid 
+                                        smallGirdLeft={<img alt="Sexual Content" style={{ maxWidth: "40px", width: "100%", height: "auto"}} src={SC_icon} />}
+                                        smallGirdRight={<AppRatings />}
+                                    />
+                                </Grid>
+
+                            </Grid>
+                        
+                            {/* Comments */}
+                            <Grid style={{ display: "flex", alignItems: "center", borderBottom: "2px solid grey", height: "54px"}}container spacing={0}>
+                                <Grid item xs={10}>
+                                    <h2 >Reviews: </h2>
+                                </Grid>
+                                <Grid align="right" item xs={2}>
+                                <Modal
+                                    modalTitle={` Share your concerns about ${data[0].name}.`}
+                                    openModal="Add Review"
+                                    modalBody={<AddAppReview/>}
+                                    modalButton1="Submit"
+                                />
+                                </Grid>
+                            </Grid>
+                            <CommentGrid
+                                // ---> Left Side
+                                imageLeft={<img alt={test_profiles[0].username} style={{  justifyContent: "center", maxWidth: "40px", width: "100%", height: "auto"}} src={test_profiles[0].image}/>}
+                                usernameLeft={test_profiles[0].username} 
+                                reviewCountLeft={test_profiles[0].reviewCount} 
+                                datePostedLeft={test_profiles[0].datePosted} 
+                                // ---> Right Side     
+                                badgesRight={<img alt="Predator Risk" style={{ maxWidth: "40px", width: "100%", height: "auto"}} src={test_profiles[0].badges}/>}
+                                dangerRatingRight={<AppRatings ratingValue={test_profiles[0].dangerRating}/>}
+                                commentsRight={test_profiles[0].comments}
                             />
-                            </Grid>
-                        </Grid>
-                        <CommentGrid
-                            // ---> Left Side
-                            imageLeft={<img alt={test_profiles[0].username} style={{  justifyContent: "center", maxWidth: "40px", width: "100%", height: "auto"}} src={test_profiles[0].image}/>}
-                            usernameLeft={test_profiles[0].username} 
-                            reviewCountLeft={test_profiles[0].reviewCount} 
-                            datePostedLeft={test_profiles[0].datePosted} 
-                            // ---> Right Side     
-                            badgesRight={<img alt="Predator Risk" style={{ maxWidth: "40px", width: "100%", height: "auto"}} src={test_profiles[0].badges}/>}
-                            dangerRatingRight={<AppRatings ratingValue={test_profiles[0].dangerRating}/>}
-                            commentsRight={test_profiles[0].comments}
-                        />
-                        <CommentGrid
-                            // ---> Left Side
-                            imageLeft={<img alt={test_profiles[1].username} style={{  justifyContent: "center", maxWidth: "40px", width: "100%", height: "auto"}} src={test_profiles[1].image}/>}
-                            usernameLeft={test_profiles[1].username}
-                            reviewCountLeft={test_profiles[1].reviewCount} 
-                            datePostedLeft={test_profiles[1].datePosted}
-                            // ---> Right Side     
-                            badgesRight={<img alt="Cyberbullying" style={{ maxWidth: "40px", width: "100%", height: "auto"}} src={test_profiles[1].badges}/>}
-                            dangerRatingRight={<AppRatings ratingValue={test_profiles[1].dangerRating}/>}
-                            commentsRight={test_profiles[1].comments}
-                        />
-                    </Wrapper>
-            </main>
-            <Footer/>
-            </>
-        )
+                            <CommentGrid
+                                // ---> Left Side
+                                imageLeft={<img alt={test_profiles[1].username} style={{  justifyContent: "center", maxWidth: "40px", width: "100%", height: "auto"}} src={test_profiles[1].image}/>}
+                                usernameLeft={test_profiles[1].username}
+                                reviewCountLeft={test_profiles[1].reviewCount} 
+                                datePostedLeft={test_profiles[1].datePosted}
+                                // ---> Right Side     
+                                badgesRight={<img alt="Cyberbullying" style={{ maxWidth: "40px", width: "100%", height: "auto"}} src={test_profiles[1].badges}/>}
+                                dangerRatingRight={<AppRatings ratingValue={test_profiles[1].dangerRating}/>}
+                                commentsRight={test_profiles[1].comments}
+                            />
+                        </Wrapper>
+                </main>
+                <Footer/>
+                </>
+            )
+        }
     }
+
+    const mapStateToProps = state => ({
+        singleApp: state.apps.singleApp
+    })
+    
+    export default connect(mapStateToProps, 
+        { 
+            viewSingleApp
+        })(App); 
+    
